702b7bd96824d35d3557571dde6c868c84859a87
Salman A <salmana.ar@gmail.com>
2021-04-26 17:03:27 +0530

    Project done

--- deploy.js
+++ deploy.js
@@ -11,7 +11,7 @@ const web3 = new Web3(provider);
 const deploy = async () => {
   const accounts = await web3.eth.getAccounts();
 
-  console.log('Attempting to deploy from account', accounts[0]);
+  console.log('Attempting to deploy from account: ', accounts[0]);
 
   const result = await new web3.eth.Contract(JSON.parse(interface))
     .deploy({ data: bytecode, arguments: ['Hello world!'] })
@@ -20,5 +20,3 @@ const deploy = async () => {
   console.log('Contract deployed to', result.options.address);
 };
 deploy();
-
-
